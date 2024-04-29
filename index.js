bc7e1c88490bca02cd572fc910b6a4de3e9b6228
Deptoha <tohakhan249156@gmail.com>
2024-04-29 17:53:42 +0600

    added view details page get oparation

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 require('dotenv').config()
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000
 
@@ -35,6 +35,13 @@ async function run() {
         app.get('/item', async (req, res) => {
             const cursor = itemCollection.find()
             const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        app.get('/item/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await itemCollection.findOne(query)
             res.send(result)
         })
 
