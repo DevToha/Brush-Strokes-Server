68cb5dba976e9ac22280c2753e7cad83e117f278
Deptoha <tohakhan249156@gmail.com>
2024-04-30 02:34:17 +0600

    added delete method

--- index.js
+++ index.js
@@ -51,6 +51,20 @@ async function run() {
             res.send(result)
         })
 
+        // app.get('/item/:id', async (req, res) => {
+        //     const id = req.params.id
+        //     const query = { _id: new ObjectId(id) }
+        //     const result = await itemCollection.findOne(query)
+        //     res.send(result)
+        // })
+
+        app.delete('/item/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await itemCollection.deleteOne(query)
+            res.send(result)
+        })
+
         app.post('/item', async (req, res) => {
             const newItem = req.body
             console.log(newItem)
